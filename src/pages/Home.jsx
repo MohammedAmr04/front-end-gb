9316d156fb124c2bbbe88e4aa642d72c4879556c
MohammedAmr04 <mohammedamr200497@gmail.com>
2024-11-30 13:21:52 +0200

    other

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import { Container } from "react-bootstrap";
 import Slider from "../components/common/Slider/Slider";
+import Landing from "../components/common/Landing/Landing";
+import Quote from "../components/common/Quote/Quote";
 
 export default function Home() {
   return (
-    <Container>
-      <Slider title={'New Arrives'} />
-      <Slider title={'New Arrives'} />
-    </Container>
-
-  )
+    <>
+      <Landing />
+      <Container>
+        <Slider title={"New Arrives"} />
+        <Quote />
+      </Container>
+    </>
+  );
 }
