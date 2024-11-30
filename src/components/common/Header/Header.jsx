adaa48165bf674d2c2e678fce917076ba9ce47ad
MohammedAmr04 <mohammedamr200497@gmail.com>
2024-11-30 16:39:29 +0200

    edit header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,31 +1,69 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import styles from "./styles.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-solid-svg-icons";
 
+const { prataRegular } = styles;
 
 export default function Header() {
   return (
-      <header>
-          <Container>
-              <h1>
-                  To Read
-              </h1>
-              {/* basket */}
-          </Container>
-          <Navbar expand="lg" className="bg-body-tertiary">
-              <Container>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse className="ms-auto" id="basic-navbar-nav">
-                      <Nav className="me-auto">
-                          <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
-                          <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
-                      </Nav>
-                      <Nav >
-                          <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
-                          <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
-                      </Nav>
-                  </Navbar.Collapse>
-              </Container>
-          </Navbar>
+    <header className={prataRegular}>
+      <Container className="d-flex justify-content-between py-2">
+        <ul className="d-flex list-unstyled">
+          <li className="mx-3">
+            <FontAwesomeIcon icon={faInstagram} />
+          </li>
+          <li className="mx-3">
+            <FontAwesomeIcon icon={faFacebook} />
+          </li>
+          <li className="mx-3">
+            <FontAwesomeIcon icon={faYoutube} />
+          </li>
+          <li className="mx-3">
+            <FontAwesomeIcon icon={faTwitter} />
+          </li>
+        </ul>
+        <div className="d-flex" role="button">
+          <div className="mx-2">
+            <FontAwesomeIcon icon={faUser} className="mx-1" />
+            account
+          </div>
+          <div className="mx-2">
+            <FontAwesomeIcon icon={faHeart} className="mx-1" /> loves
+          </div>
+        </div>
+      </Container>
+      <hr />
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/profile">
+                Profile
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={NavLink} to="/login">
+                Login
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/register">
+                Register
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
+  );
 }
--- src/components/common/Slider/Slider.jsx
+++ src/components/common/Slider/Slider.jsx
@@ -3,17 +3,19 @@ import { Container } from "react-bootstrap";
 import HeaderSection from "../HeaderSection/HeaderSection";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import styles from "./styles.module.css";
 
+const { widthFit } = styles;
 export default function Slider() {
   const books = Array.from({ length: 4 }, (_, index) => ({
     id: index + 1,
   }));
 
   return (
-    <Container className=" position-relative">
+    <Container className=" position-relative py-5">
       <HeaderSection />
       {/* Rendering CardBook components */}
-      <div className="d-flex justify-content-center align-items-center flex-wrap">
+      <div className="d-flex justify-content-center align-items-center flex-wrap ">
         {books.map((book) => (
           <div key={book.id} className="mx-2">
             <CardBook />
@@ -21,7 +23,7 @@ export default function Slider() {
         ))}
       </div>
       <hr />
-      <p role="button" className=" text-end">
+      <p role="button" className={`position-absolute end-0 py-3 ${widthFit}`}>
         Veiw all books <FontAwesomeIcon className=" px-1" icon={faArrowRight} />
       </p>
     </Container>
